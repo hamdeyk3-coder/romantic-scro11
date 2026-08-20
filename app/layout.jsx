8051d0f8bcf933a2197ddd35417bb2bfec6f2498
hamdeyk3-coder <hamdeyk3@gmail.com>
2026-08-20 04:31:59 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,9 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
+    export default function RootLayout({
+      children,
+    }) {
+      return (
+        <html lang="ar">
+          <body>{children}</body>
+        </html>
+      )
+    }
